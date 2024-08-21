eaee35ce9e989bdbc61563b7729ceb1153aeead0
Keshav Thosar <46895675+KeshavThosar@users.noreply.github.com>
2024-08-22 04:31:19 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -72,6 +72,7 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -86,6 +87,7 @@ def get_accounts(account_id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
